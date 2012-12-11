11e0e87579c8f5e2980c1859a9ff3f29a94cabf0
Ken Hoff <thekenhoff@gmail.com>
2012-12-10 23:08:04 -0700

    new monster truck model, added in-air maneuvers

---
+++ Assets/InAirCar.js
@@ -0,0 +1,22 @@
+#pragma strict
+
+public var force = 10000;
+
+function Start () {
+
+}
+
+function Update () {
+	if (Input.GetKey(KeyCode.I)) {
+		rigidbody.AddTorque(Vector3.right * force);
+	}
+	if (Input.GetKey(KeyCode.K)) {
+		rigidbody.AddTorque(-Vector3.right * force);
+	}
+	if (Input.GetKey(KeyCode.J)) {
+		rigidbody.AddTorque(Vector3.forward * force);
+	}
+	if (Input.GetKey(KeyCode.L)) {
+		rigidbody.AddTorque(-Vector3.forward * force);
+	}
+}
